3696d1f5b768fd25316a57ff304d5ee30abf0304
Ralph Leyga <ralphfleyga@gmail.com>
2018-01-30 12:46:37 +0800

    initial flask setup

---
+++ README.md
@@ -0,0 +1,8 @@
+Requirements:
+* Python 3.5.x
+* Flask
+
+Setup:
+* Run `pip install -r requirements.txt`
+* Run `export FLASK_APP=mainapp.py`
+* To run the app, `flask run`
--- js/animatecss/animate.js
+++
@@ -1,8 +0,0 @@
-/**
- * Reveal Search Input
- */
-$(document).ready(function() {
-    $('#btn-voice-search').click(function() {
-        $('#voice-search-modal').addClass('open animated fadeIn');
-    });
-});
